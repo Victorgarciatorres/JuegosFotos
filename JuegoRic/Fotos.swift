17484d56d2a89980c59af15681868a20482cb2f3
VictorGarcia <victor_garcia_apps1ma1819@cev.com>
2019-10-15 10:17:42 +0200

    Victor comienzo juego

---
+++ JuegoRic/Fotos.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class Fotos: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBOutlet weak var allPhotos: UICollectionView!
+    
+    
+}
+
